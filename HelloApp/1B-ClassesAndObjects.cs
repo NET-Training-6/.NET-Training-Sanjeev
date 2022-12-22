2040ffea385568c2eae476be3fff45ddc2430aa2
xanjvstha <xanjvstha@gmail.com>
2022-12-22 19:05:34 +0545

    class

---
+++ HelloApp/1B-ClassesAndObjects.cs
@@ -0,0 +1,18 @@
+class Animal
+{
+    //Members: methods, fields
+    // string name;
+    // byte age;
+    // string address;
+    // float height;
+    // DateTime dob;
+    // void CalculateBMI
+    String name;
+    byte age;
+    byte legs;
+    String type
+    string color
+
+
+
+}
